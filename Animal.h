097b92db025610dc22a227a410765b8a9677c1a7
MostafaElhodaiby <mostafaelhodaiby11@gmail.com>
2025-11-12 15:04:32 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -1,11 +1,20 @@
 #ifndef ANIMAL_H
 #define ANIMAL_H
 
-#include <string>
-using namespace std;
-
-//define animal class here 
-
-
-
+class Animal{
+    private:
+    string name;
+    int age;
+    bool Ishungry;
+    public:
+    Animal();
+    
+    Animal(string n , int a, bool I);
+    
+     ~Animal();
+    void display();
+    void feed();
+    
+    
+};
 #endif
